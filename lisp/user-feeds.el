7e92772e9f824311aee80c5f15d182b051902786
Anders Dalskov <anderspkd@gmail.com>
2016-09-07 13:29:29 +0200

    Added some colors to elfeed entries.

--- lisp/user-feeds.el
+++ lisp/user-feeds.el
@@ -1,5 +1,29 @@
 (require 'user-secrets)
 
+;; Don't tag old entries
+(add-hook 'elfeed-new-entry-hook
+          (elfeed-make-tagger :before "1 month ago"
+                              :remove 'unread))
+
+;;; Some settings relating to faces
+(defface reddit-elfeed-entry
+  '((t :foreground "#2874a6"))
+  "colors for 'reddit tagged entries")
+(push '(reddit reddit-elfeed-entry)
+      elfeed-search-face-alist)
+
+(defface advisory-elfeed-entry
+  '((t :foreground "#58d68d"))
+  "colors for 'advisory tagged entries")
+(push '(advisory advisory-elfeed-entry)
+      elfeed-search-face-alist)
+
+(defface youtube-elfeed-entry
+  '((t :foreground "#E74C3C"))
+  "colors for 'youtube tagged entries")
+(push '(youtube youtube-elfeed-entry)
+      elfeed-search-face-alist)
+
 (defun expand-feed (feed)
   (cl-destructuring-bind (link . tags) feed
     (cond ((member 'youtube tags)
