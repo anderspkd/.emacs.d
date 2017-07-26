18974791908801cd7a95c18985e876a05b8408ad
Anders Dalskov <anderspkd@gmail.com>
2017-07-26 11:51:56 +0200

    renamed notify package.

--- lisp/dbus-notify.el
+++ lisp/dbus-notify.el
@@ -22,17 +22,22 @@
 (require 'dbus)
 (require 'cl-macs)
 
-(eval-and-compile ; shuts up complaints about void definition in the two macros below.
+(eval-and-compile
+  ;;; Notification service, interface and path variables
   (defvar notify/service "org.freedesktop.Notifications")
   (defvar notify/interface "org.freedesktop.Notifications")
   (defvar notify/path "/org/freedesktop/Notifications")
+
+  ;;; List holding events that should be fired when a notification is
+  ;;; closed.
   (defvar notify/on-close-events nil)
   (defvar notify/on-close-handler-registered nil)
+
+  ;;; List holding events that should be fired when an action is
+  ;;; invoked on a notification.
   (defvar notify/actions nil)
   (defvar notify/action-handler-registered nil))
 
-;; A lot of parameter re-use goes into `dbus-call-method' and
-;; `dbus-register-signal', which these two macros deal with.
 (defmacro notify/call-method (&rest args)
   (declare (indent defun))
   `(dbus-call-method :session ,notify/service ,notify/path ,notify/interface ,@args))
@@ -51,12 +56,13 @@
 
 (defun notify/signal-handler (notification-id action-id)
   (let ((action (assoc action-id notify/actions)))
-    (pcase action
-      (`(,name ,fun . ,args)
-       (progn
-	 (message "(notify) action %s invoking %s on %s" name fun args)
-	 (apply fun args)))
-      (_ (message "(notify) signal was invoked for unknown action: %s %s" notification-id action-id)))))
+    (when action
+      (pcase action
+	(`(,name ,fun . ,args)
+	 (progn
+	   (message "(notify) action %s invoking %s on %s" name fun args)
+	   (apply fun args)))
+	(_ (message "(notify) signal was invoked for unknown action: %s %s" notification-id action-id))))))
 
 (defun notify/setup-action-handler ()
   (unless notify/action-handler-registered
@@ -71,8 +77,8 @@
 		      (2 :dismissed)
 		      (3 :closed)
 		      (4 :undefined))))
-	(funcall (cdr event-fun) reason))
-      (setq notify/on-close-events (assq-delete-all notification-id notify/on-close-events)))))
+	(funcall (cdr event-fun) reason)
+	(setq notify/on-close-events (assq-delete-all notification-id notify/on-close-events))))))
 
 (defun notify/setup-on-close-event-handler ()
   (unless notify/on-close-handler-registered
@@ -91,16 +97,21 @@
 	(location (plist-get args :location))
 	(urgency (plist-get args :urgency))
 	hints)
-    ;;; Register the action handler
+
+    ;; Setup handlers. Note that both are essentially no-ops if no
+    ;; actions are specified.
     (notify/setup-action-handler)
-    ;;; Set location (dunno if it works)
+    (notify/setup-on-close-event-handler)
+
+    ;; Set location (has not been tested)
     (when location
       (if (listp location)
 	  (cl-destructuring-bind (x . y) location
 	    (push `(:dict-entry "x" (:variant :int32 ,x)) hints)
 	    (push `(:dict-entry "y" (:variant :int32 ,y)) hints))
 	(error "(notify) location not of the form '(x . y)")))
-    ;;; Set urgency
+
+    ;; Set urgency
     (when urgency
       (push `(:dict-entry "urgency"
 			  (:variant :byte ,(pcase urgency
@@ -109,9 +120,11 @@
 					     ('critical 2)
 					     (_ 1))))
 	    hints))
-    ;;; If no hints was set, set to default (i.e., empty dict)
+
+    ;; If no hints was set, set to default (i.e., empty dict)
     (unless hints
       (setq hints '(:array :signature "{sv}")))
+
     ;; process action(s)
     (dolist (action action-list)
       (cl-destructuring-bind (fun msg . args) action
@@ -119,16 +132,19 @@
 	  (add-to-list 'notify/actions
 		       (cons action-name (cons fun args)))
 	  (setq actions (append actions (list action-name msg))))))
-    ;; fire the notification and setup the on-close-event, if supplied.
+
+    ;; Show notification and add the on-close-event, if supplied. We
+    ;; have to add the on-close-event here, since we need the
+    ;; notification-id to see which notification uses which event.
     (let ((notification-id (notify/call-method "Notify" app-name replace-id "" summary msg actions hints :int32 timeout)))
       (when (and on-close-event (functionp on-close-event))
-	(notify/setup-on-close-event-handler)
 	(push (cons notification-id on-close-event) notify/on-close-events))
       notification-id)))
 
-(with-eval-after-load "asd-notify"
-  (unless (dbus-ping :session notify/service)
-    (error "dbus seems to be dead... Notifications might not work :-(")))
+;;;; Takes to long to run (several ms!!)
+;; (with-eval-after-load "asd-notify"
+;;   (unless (dbus-ping :session notify/service)
+;;     (error "dbus seems to be dead... Notifications might not work :-(")))
 
-(provide 'asd-notify)
+(provide 'dbus-notify)
 ;;; asd-notify.el ends here
