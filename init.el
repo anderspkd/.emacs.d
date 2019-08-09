d78c504af6cca27b68ae22ec330f303defd26503
Anders Dalskov <anderspkd@fastmail.com>
2019-08-09 10:52:47 +0200

    flagging emails.

--- init.el
+++ init.el
@@ -569,6 +569,9 @@ prepended to the guard."
   (add-to-list 'mu4e-view-actions
 	       '("ViewInBrowser" . mu4e-action-view-in-browser))
 
+  (add-to-list 'mu4e-bookmarks '("flag:flagged" "Flagged Messages" ?f))
+
+  (add-hook 'mu4e-compose-mode-hook (lambda () (setq fill-column 70)))
   (setq mu4e-user-mail-address-list `(,private-mail-address ,work-mail-address))
   (setq mail-user-agent 'mu4e-user-agent)
   (setq mu4e-maildir "~/mail")
