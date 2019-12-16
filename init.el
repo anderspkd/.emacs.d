ff372382d6c808d5bfb02e2aa98bbf067b8fb213
Anders Dalskov <anderspkd@fastmail.com>
2019-12-16 10:38:54 +0100

    proper indentation for c++ lambdas.

--- init.el
+++ init.el
@@ -322,7 +322,8 @@ prepended to the guard."
   :init
   (trim-ws-in-mode 'c++-mode)
   (defconst cpp-no-ns-indent
-    '("linux" (c-offsets-alist . ((innamespace . [0])))))
+    '("linux" (c-offsets-alist . ((innamespace . [0])
+				  (inlambda . 0)))))
   (c-add-style "cpp-no-ns-indent" cpp-no-ns-indent)
   (add-hook 'c++-mode-hook (lambda ()
 			     (c-set-style "cpp-no-ns-indent")
