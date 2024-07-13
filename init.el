f61455068c627b19effd4a0c5da52878cffbefe7
Anders Dalskov <anderspkd@secata.com>
2024-07-13 21:58:15 +0200

    init up

--- init.el
+++ init.el
@@ -1,110 +1,70 @@
-(defvar old-file-name-handler-alist file-name-handler-alist)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")))
 
-(setq gc-cons-threshold 64000000
-      auto-save-list-file-prefix nil
-      package-enable-at-startup nil
-      package--init-file-ensured t
-      file-name-handler-alist nil
-      package-user-dir "~/.emacs.d/elpa/"
-      package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("gnu" . "https://elpa.gnu.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(add-hook 'after-init-hook
-	  #'(lambda ()
-	      (setq gc-cons-threshold 800000
-		    file-name-handler-alist old-file-name-handler-alist)))
+(require 'use-package)
+(require 'bind-key)
 
-(mapc #'(lambda (add) (add-to-list 'load-path add))
-      (eval-when-compile
-        (package-initialize)
-        (unless (package-installed-p 'use-package)
-          (package-refresh-contents)
-          (package-install 'use-package))
-        (setq use-package-always-ensure t)
-        (let ((package-user-dir-real (file-truename package-user-dir)))
-          ;; The reverse is necessary, because outside we mapc
-          ;; add-to-list element-by-element, which reverses.
-          (nreverse (apply #'nconc
-                           ;; Only keep package.el provided loadpaths.
-                           (mapcar #'(lambda (path)
-                                       (if (string-prefix-p package-user-dir-real path)
-                                           (list path)
-                                         nil))
-                                   load-path))))))
+;; general configuration
+(defun apkd-emacs-dir (&optional filename)
+  (expand-file-name (concat user-emacs-directory filename)))
 
-(use-package bind-key)
-(use-package diminish)
-;; wack!
-(use-package use-package :commands use-package-autoload-keymap)
+(setq custom-file (apkd-emacs-dir "custom.el"))
+(load custom-file)
 
-(setf (symbol-function 'x-focus-frame) #'ignore)
+(load (apkd-emacs-dir "settings.el"))
 
-(eval-and-compile
-  (defsubst emacs-dir (&optional file-name)
-    (expand-file-name (concat user-emacs-directory file-name)))
+;; look and feel
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
-  ;; load custom directory and folder name variables
-  (load-file (emacs-dir (concat "personal/settings")))
+(setq indent-tabs-mode nil)
+(setq fill-column 80)
 
-  (setq custom-file (emacs-dir "custom.el"))
-  (load custom-file)
+(line-number-mode t)
+(column-number-mode t)
+(electric-pair-mode t)
 
-  (setq-default indent-tabs-mode nil)
+(fset 'yes-or-no-p 'y-or-n-p)
 
-  (add-to-list 'load-path (emacs-dir "etc")))
+(setq initial-scratch-message nil
+      ring-bell-function 'ignore
+      inhibit-startup-screen t
+      scroll-step 1)
 
-(eval-and-compile
+(setq backup-by-copying t
+      backup-directory-alist `(("." . ,(apkd-emacs-dir "backups")))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
-  ;; Remove toolbar, menu etc...
-  (tool-bar-mode -1)
-  (menu-bar-mode -1)
-  (scroll-bar-mode -1)
-  (blink-cursor-mode -1)
-  (line-number-mode t)
-  (column-number-mode t)
-  (electric-pair-mode t)
+(setq split-height-threshold 120
+      split-width-threshold 160)
 
-  (setq-default fill-column 80)
+(set-face-attribute 'default nil :height 90 :font "DejaVu Sans Mono")
 
-  (setq initial-scratch-message nil
-	ring-bell-function 'ignore
-	inhibit-startup-screen t
-	scroll-step 1)
+(setq solarized-distinct-fringe-background t
+      solarized-use-variable-pitch nil
+      solarized-emphasize-indicators nil
+      solarized-scale-org-headlines nil
+      solarized-scale-markdown-headlines t
+      solarized-height-minus-1 1.0
+      solarized-height-plus-1 1.0
+      solarized-height-plus-2 1.0
+      solarized-height-plus-3 1.0
+      solarized-height-plus-4 1.0)
 
-  (fset 'yes-or-no-p 'y-or-n-p)
+(load-theme 'solarized-light t)
 
-  (setq backup-by-copying t
-	backup-directory-alist `(("." . ,asd::folders::emacs-backups))
-	delete-old-versions t
-	kept-new-versions 6
-	kept-old-versions 2
-	version-control t)
-
-  (defvar trim-ws-modes nil
-    "Modes in which whitespace should automatically be trimmed")
-
-  (defun enable-automatic-whitespace-trimming (mode)
-    "Automatically trim ws in MODE. if NO-TRIM is non-nil, then
-MODE disable ws trimming."
-    (unless (memq mode trim-ws-modes)
-      (push mode trim-ws-modes)))
-
-  (defun smart-whitespace-trim ()
-    (when (memq major-mode trim-ws-modes)
-      (delete-trailing-whitespace)))
-
-  (add-hook 'before-save-hook 'smart-whitespace-trim t))
-
-(bind-key "C-c C-d"
-          (lambda ()
-            (interactive)
-            (move-beginning-of-line 1)
-            (kill-line)
-            (yank)
-            (open-line 1)
-            (next-line 1)
-            (yank)))
+(setq frame-background-mode nil)
 
 ;; https://emacs.stackexchange.com/a/37648
 (defun replace-or-delete-pair (open)
@@ -144,15 +104,6 @@ an error."
   (delete-pair)
   (backward-char 1))
 
-(bind-key "C-c q" 'quick-find-directory)
-
- (defun quick-find-directory (directory)
-    (interactive
-     (let* ((hist (mapcar 'car asd::folders::quick-dirs))
-	    (choice (completing-read "directory: " hist)))
-       (list (cdr (assoc choice asd::folders::quick-dirs)))))
-    (dired directory))
-
 (bind-key "C-c e" 'replace-or-delete-pair)
 
 (use-package ace-window
@@ -160,110 +111,77 @@ an error."
   :config
   (bind-key "C-x o" 'ace-window))
 
-(use-package elfeed
-  :ensure t
-  :defer t
-  :config
-  (setq elfeed-feeds asd::rss-feeds))
-
-(use-package dired
-  :ensure nil  ; dired is already installed by default
-  :defer t
-  :bind (:map dired-mode-map
-	      ([backspace] . dired-up-directory)
-	      ("b" . browse-url-of-dired-file)
-	      ("\"" . do-shell-and-copy-to-kill-ring))
-  :init
-  (add-hook 'dired-mode-hook 'toggle-truncate-lines)
-  (add-hook 'dired-mode-hook 'dired-omit-mode)
-  (add-hook 'dired-mode-hook 'dired-hide-details-mode)
-  :config
-  (use-package dired-x :ensure nil)  ; so is dired-x
-  (setq-default dired-omit-files-p t)
-  (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$\\|__pycache__"))
-  (setq dired-auto-revert-buffer t
-	;; no "." and "..", long-list, human readable, classify, dirs first
-	dired-listing-switches "-AlhF --group-directories-first")
-
-  ;; https://stackoverflow.com/a/29816147
-  (defun do-shell-and-copy-to-kill-ring (command &optional arg file-list)
-    (interactive
-     (let ((files (dired-get-marked-files t current-prefix-arg)))
-       (list
-	(dired-read-shell-command "! on %s: " current-prefix-arg files)
-	current-prefix-arg
-	files)))
-    (dired-do-shell-command command arg file-list)
-    (with-current-buffer "*Shell Command Output*"
-      (copy-region-as-kill (point-min) (point-max)))))
+(use-package yasnippet
+  :ensure t)
 
 (use-package magit
   :ensure t)
 
-(use-package yasnippet
-  :ensure t)
+(use-package ivy
+  :ensure t
+  :init
+  (setq ivy-use-selectable-prompt t))
+
+(ivy-mode t)
+
+(defun apkd-do-shell-and-copy (command &optional arg file-list)
+  (interactive
+   (let ((files (dired-get-marked-files t current-prefix-arg)))
+     (list
+      (dired-read-shell-command "! on %s: " current-prefix-arg files)
+      current-prefix-arg
+      files)))
+  (dired-do-shell-command command arg file-list)
+  (with-current-buffer "*Shell Command Output*"
+    (copy-region-as-kill (point-min) (point-max))))
+
+(use-package dired
+  :defer t
+  :bind (:map dired-mode-map
+              ([backspace] . dired-up-directory)
+              ("b" . browse-url-of-dired-file)
+              ("\"" . apkd-do-shell-and-copy))
+  :hook ((dired-mode . toggle-truncate-lines)
+         (dired-mode . dired-omit-mode)
+         (dired-mode . dired-hide-details-mode))
+  :config
+  (require 'dired-x)
+
+  (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$\\|__pycache__")
+        dired-auto-revert-buffer t
+        dired-listing-switches "-AlhF --group-directories-first"))
 
 (use-package org
   :mode ("\\.org\\'" . org-mode)
   :bind (("C-c a" . org-agenda)
-	 ("C-c C-l" . org-store-link)
-	 ("C-c c" . org-capture)
-	 ("C-c i" . org-insert-link))
+         ("C-c C-l" . org-store-link)
+         ("C-c l" . org-insert-link))
+  :hook ((org-mode . yas-minor-mode))
   :config
-  (yas-minor-mode)
-  (enable-automatic-whitespace-trimming 'org-mode)
-	     
-  (setq org-agenda-files asd::folders::org-files
-	org-log-reschedule t
-	org-log-done t))
+
+  (require 'solarized-palettes)
+  (let ((green (cdr (assoc 'green solarized-light-color-palette-alist)))
+	(blue (cdr (assoc 'blue solarized-light-color-palette-alist)))
+	(yellow (cdr (assoc 'yellow solarized-light-color-palette-alist)))
+	(red (cdr (assoc 'red solarized-light-color-palette-alist))))
+    (setq org-todo-keyword-faces
+	  `(("DONE" . (:foreground ,green :weight bold))
+	    ("TODO" . (:foreground ,blue :weight bold))
+	    ("REFINE" . (:foreground ,yellow :weight bold))
+	    ("DOING" . (:foreground ,red :weight bold)))))
+
+  (setq org-agenda-files apkd-folders-org-agenda-files
+        org-log-reschedule t
+	org-adapt-indentation nil
+        org-log-done t))
 
 (use-package projectile
   :ensure t
+  :defer nil
   :bind (("C-c p" . projectile-commander))
   :config
   (require 'subr-x)
   (projectile-mode 1))
-
-(use-package hydra
-  :ensure t)
-
-(use-package multiple-cursors
-  :ensure t
-  :bind ("C-c m" . hydra-mc/body)
-  :config
-  (defhydra hydra-mc (:hint nil)
-    "
-Mark multiple things (_n_) next item, (_p_) previous item. (_q_) quit"
-    ("n" mc/mark-next-like-this)
-    ("p" mc/mark-previous-like-this)
-    ("q" nil)))
-
-(defhydra hydra-resize-windows (global-map "C-c r")
-  "Resize buffer"
-  ("h" (lambda (n) (interactive "p") (dotimes (i n) (shrink-window 3 t))))
-  ("l" (lambda (n) (interactive "p") (dotimes (i n) (shrink-window -3 t))))
-  ("j" (lambda (n) (interactive "p") (dotimes (i n) (shrink-window -3))))
-  ("k" (lambda (n) (interactive "p") (dotimes (i n) (shrink-window 3))))
-  ("q" nil))
-
-(use-package python
-  :mode (("\\.py\\'" . python-mode)
-	 ("\\.mpc\\'". python-mode))
-  :interpreter ("python" . python-mode)
-  :bind (:map python-mode-map
-	      ("C-c b t" . hs-toggle-hiding)
-	      ("C-." . dabbrev-expand))
-  :init
-  (add-hook 'python-mode-hook (lambda () (hs-minor-mode 1)))
-  (add-hook 'python-mode-hook 'yas-minor-mode)
-  (add-hook 'python-mode-hook 'nlinum-mode)
-  (enable-automatic-whitespace-trimming 'python-mode)
-  :config
-  (setq python-shell-interpreter "ipython"
-	python-shell-interpreter-args "-i --simple-prompt"
-	python-indent-offset 4))
-
-;; (pdf-tools-install :no-query)
 
 (use-package pdf-tools
   :ensure t
@@ -295,121 +213,62 @@ Mark multiple things (_n_) next item, (_p_) previous item. (_q_) quit"
   (setq reftex-plug-into-AUCTeX t
 	reftex-ref-style-default-list '("Default" "Hyperref")))
 
-(require 'ansi-color)
-(defun colorize-compilation-buffer ()
-  (ansi-color-apply-on-region compilation-filter-start (point)))
-(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
-(setq ansi-color-names-vector
-      ["#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"])
-
 (defun insert-header-guard (prefix)
-  "Insert a C/C++ style header guard with a name derived from the
-filename."
   (interactive
    (list (read-string "prefix: ")))
-  (let ((ext (file-name-extension (buffer-file-name))))
-    (let ((is-cpp (string= ext "hpp"))
-	  (is-c (string= ext "h")))
-      (unless (or is-cpp is-c)
-	(error "Not in a c or c++ header file ... "))
-      (let* ((filename (file-name-base (buffer-file-name)))
-	     guard)
-	(when (stringp filename)
-	  (if prefix
-	      (setq guard (format "%s_%s_%s" (upcase prefix) (upcase filename) (upcase ext)))
-	    (setq guard (format "%s_%s" (upcase filename) (upcase ext))))
-	  (insert (format "#ifndef %s\n" guard))
-	  (insert (format "#define %s\n\n" guard))
-	  (forward-line)
-	  (save-excursion
-	    (goto-char (point-max))
-	    (insert (format "\n\n#endif %s" (format "// %s" guard)))))))))
+  (let ((ext (file-name-extension (buffer-file-name)))
+	((filename (file-name-base (buffer-file-name))))
+	guard)
+    (when (stringp filename)
+      (if (string-empty-p prefix)
+	  (setq guard (format "%s_%s" (upcase filename) (upcase ext)))
+	(setq guard (format "%s_%s_%s" (upcase prefix) (upcase filename) (upcase ext))))
+      (insert (format "#ifndef %s\n" guard))
+      (insert (format "#define %s\n\n" guard))
+      (forward-line)
+      (save-excursion
+	(goto-char (point-max))
+	(insert (format "\n\n#endif %s" (format "// %s" guard)))))))
 
-(defconst clang-format-file-of-buffer-p t)
+(use-package company
+  :ensure t)
 
-(defun clang-format-file-of-buffer ()
-  (interactive)
-  (when clang-format-file-of-buffer-p
-    (message "formatting buffer with clang-format")
-    (let ((filename (buffer-file-name)))
-      (call-process "clang-format" nil nil nil filename "-i" "--style=Google")
-      (revert-buffer nil t t))))
-
-(bind-key "C-c f" 'clang-format-file-of-buffer)
-
-(use-package ivy
+(use-package eglot
   :ensure t
   :init
-  (setq ivy-use-selectable-prompt t))
+  (add-hook 'eglot-managed-mode-hook
+	    (lambda () (eglot-inlay-hints-mode -1))))
 
-(ivy-mode t)
+(use-package modern-cpp-font-lock
+  :ensure t)
+
+(defconst apkd-cpp-no-namespace-indent
+  '("linux" (c-offsets-alist . ((innamespace . [0])
+                                (topmost-intro-cont 0 nil)
+                                (access-label -1)
+				(inlambda . 0)))))
+(c-add-style "apkd-cpp-no-namespace-indent" apkd-cpp-no-namespace-indent)
+
+(use-package cmake-mode
+  :ensure t)
 
 (use-package c++-mode
-  :ensure nil
+  :defer nil
   :mode ("\\.cpp\\'" "\\.h\\'")
-  :bind (:map c++-mode-map              
-	      ([ret] . newline-and-indent))
+  :bind (([ret] . newline-and-indent)
+	 ("C-c C-f" . eglot-format)
+	 ("C-c C-r" . eglot-rename)
+	 ("C-c f n" . flymake-goto-next-error)
+	 ("C-c f p" . flymake-goto-prev-error)
+	 ("C-c f d" . eglot-find-declaration))
+  :hook ((c++-mode . (lambda ()
+		       (c-set-style "apkd-cpp-no-namespace-indent")
+		       (setq c-basic-offset 2)))
+	 (c++-mode . eglot-ensure)
+	 (c++-mode . yas-global-mode))
   :init
-  (use-package modern-cpp-font-lock :ensure t)
-  (projectile-mode 1)
-  (modern-c++-font-lock-global-mode t)
-  (enable-automatic-whitespace-trimming 'c++-mode)
-  ;; define a custom c++ style where namespaces and lambdas have no indentation.
-  (defconst cpp-no-ns-indent
-    '("linux" (c-offsets-alist . ((innamespace . [0])
-                                  (topmost-intro-cont 0 nil)
-                                  (access-label -1)
-				  (inlambda . 0)))))
-  (c-add-style "cpp-no-ns-indent" cpp-no-ns-indent)
-  (add-hook 'c++-mode-hook (lambda ()
-			     (c-set-style "cpp-no-ns-indent")
-			     (setq c-basic-offset 2)
-                             (setq indent-tabs-mode nil)
-                             (define-key c++-mode-map (kbd "C-c C-d") nil)))
-  (setq lsp-clients-clangd-args '("--fallback-style=Google"))
-  (add-hook 'c++-mode-hook 'lsp)
-  (add-hook 'c++-mode-hook (lambda () (setq fill-column 80)))
-  (add-hook 'c++-mode-hook (lambda () (yas-global-mode 1)))
-  :config
-  (setq lsp-enable-on-type-formatting nil))
+  (modern-c++-font-lock-global-mode t))
 
-(use-package lsp
-  :ensure nil
-  :init
-  (add-hook 'lsp-mode-hook (lambda ()
-                             (let ((lsp-keymap-prefix "C-c l"))
-                               'lsp-enable-which-key-integration)))
-  (require 'dap-cpptools)
-  (yas-global-mode)
-  :config
-  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map))
-
-(eval-and-compile
-  (require 'iso-transl)
-  (set-face-attribute 'default nil :height 90 :font "DejaVu Sans Mono")
-  
-  (require 'color-theme-sanityinc-tomorrow)
-  (color-theme-sanityinc-tomorrow 'night)
-
-  (setq frame-background-mode nil)
-  
-  ;; Fun with bitmaps :-)
-  (define-fringe-bitmap 'right-curly-arrow
-    [#b00000000
-     #b00000000
-     #b00000000
-     #b01111100
-     #b01111100
-     #b00001100
-     #b00001100
-     #b00000000])
-
-  (define-fringe-bitmap 'left-curly-arrow
-    [#b00000000
-     #b00110000
-     #b00110000
-     #b00111110
-     #b00111110
-     #b00000000
-     #b00000000
-     #b00000000]))
+(use-package elpy
+  :ensure t
+  :init (elpy-enable))
