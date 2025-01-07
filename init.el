cee3f34d953ea0c0236a03d18e7f8be5d30fa189
Anders Dalskov <anderspkd@fastmail.com>
2025-01-07 11:19:39 +0100

    give up on elfeed

--- init.el
+++ init.el
@@ -230,20 +230,6 @@ an error."
         org-hide-emphasis-markers t
         org-log-done t))
 
-(use-package elfeed
-  :ensure t
-  :init
-  (setq elfeed-search-title-max-width 160)
-  (setq elfeed-feeds (apkd-get-setting :feeds)))
-
-(use-package elfeed-extras
-  :after elfeed
-  :config
-  (dolist (tag-and-face (apkd-get-setting :feeds-tag-faces))
-    (let ((tag (car tag-and-face))
-          (face (cdr tag-and-face)))
-      (elfeed-extras-defface-for-tag tag face))))
-
 (use-package hydra
   :ensure t
   :defer nil
